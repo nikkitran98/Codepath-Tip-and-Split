4293ad7ba715e3785e6219a3110310cc65fffd13
Nikki Tran <nikkitran@fb.com>
2018-06-26 13:20:23 -0700

    Initial Commit

---
+++ tippy/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  tippy
+//
+//  Created by Nikki Tran on 6/26/18.
+//  Copyright © 2018 Nikki Tran. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ tippyTests/tippyTests.m
@@ -0,0 +1,39 @@
+//
+//  tippyTests.m
+//  tippyTests
+//
+//  Created by Nikki Tran on 6/26/18.
+//  Copyright © 2018 Nikki Tran. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface tippyTests : XCTestCase
+
+@end
+
+@implementation tippyTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
